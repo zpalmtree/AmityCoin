e1a8eb1e1cd36450672462953a58ae3388f4f1a7
Amity <42378180+Amity-Core@users.noreply.github.com>
2018-09-05 22:39:50 +0100

    This *should* be fine

--- deps.sh
+++ deps.sh
@@ -1,10 +1,13 @@
 # Found from the Turtlecoin GitHub
 # Copyright (c) The TurtleCoin Developers
 # Copyright (c) The Amity Developers
+#!/bin/sh
 
-sudo apt-get update 
-yes "" | sudo apt-get install build-essential python-dev gcc gcc-7 cpp g++ cpp-7 g++-7 git cmake libboost1.58-all-dev librocksdb-dev 
-export CXXFLAGS="-std=gnu++11" 
-mkdir build && cd $_ 
-cmake .. 
-make
+# This is installing the dependencie's
+sudo apt-get update -y
+sudo apt-get install -y build-essential python-dev gcc g++ git cmake libboost-all-dev zip
+# This is building AmityCoin
+export CXXFLAGS="-std=gnu++11"
+sudo mkdir build && cd build
+sudo cmake ..
+sudo make -j4
